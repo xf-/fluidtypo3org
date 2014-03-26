b0f75e5bbd3e71c70766506524081e0a041e5756
Claus Due <claus@namelesscoder.net>
2014-03-26 14:40:37 +0100

    [TASK] Delete unused TCA additions

--- ext_tables.php
+++ ext_tables.php
@@ -6,11 +6,3 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Provider extension for pages content');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('FluidTYPO3.Fluidtypo3org', 'Page');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('FluidTYPO3.Fluidtypo3org', 'Content');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'tx_fluidtypo3org_icon');
-$GLOBALS['TCA']['pages']['columns']['tx_fluidtypo3org_icon'] = array(
-	'label' => 'Icon',
-	'config' => array(
-		'type' => 'input'
-	)
-);
